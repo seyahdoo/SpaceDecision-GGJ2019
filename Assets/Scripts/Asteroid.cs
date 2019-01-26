6df62138b724739b1a55e363b846e94f6bc95a4d
Berkay Dursun <a.berkay.dursun@windowslive.com>
2019-01-26 23:15:27 +0300

    New ship

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float RotationSpeed;
+
+    Transform EarthTransform;
+
+    void Awake()
+    {
+        EarthTransform = GameObject.Find("Earth").transform;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 directionVector = (EarthTransform.position - transform.position).normalized;
+        transform.position += directionVector * MoveSpeed * Time.deltaTime;
+        transform.Rotate(transform.up, RotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float RotationSpeed;
+    
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
