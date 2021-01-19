5cb3a62c1e53130ad0773cff27cc9ed2e15a9523
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-01-19 18:48:25 +0300

    forgotten changes

---
+++ Assets/BedController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using seyahdoo.crosshair;
+
+public class BedController : Target
+{
+
+    public float triggerDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void FocusOn()
+    {
+
+        base.FocusOn();
+    }
+
+    public override void FocusStay()
+    {
+        base.FocusStay();
+
+        
+
+    }
+
+    public override void FocusOff()
+    {
+        base.FocusOff();
+    }
+}
---
+++ Assets/DoorScipt.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoorScipt : MonoBehaviour
+{
+
+    public Transform openState;
+    public Transform closeState;
+    public Transform playerPos;
+    public Transform doorPos;
+    public float openDistance;
+
+    public bool opened;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Vector3.Distance(playerPos.position, doorPos.position) < openDistance)
+        {
+            if (!opened)
+            {
+                opened = true;
+                StartCoroutine(OpenDoor());
+            }
+        }
+        else
+        {
+            if (opened)
+            {
+                opened = false;
+                StartCoroutine(CloseDoor());
+            }
+        }
+
+    }
+
+    IEnumerator OpenDoor()
+    {
+        float f = 0;
+        Vector3 st = transform.position;
+        while (f < 1)
+        {
+            f += Time.deltaTime / .20f;
+            transform.position = Vector3.Lerp(st, openState.position, f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = openState.position;
+
+    }
+
+    IEnumerator CloseDoor()
+    {
+        float f = 0;
+        Vector3 st = transform.position;
+        while (f < 1)
+        {
+            f += Time.deltaTime / .20f;
+            transform.position = Vector3.Lerp(st, closeState.position, f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = closeState.position;
+    }
+    
+}
